e57778fa988a3e9154b274a8fcaf9231220a385a
Anthony Limani <77291582+broverlord93@users.noreply.github.com>
2021-12-22 10:36:38 -0500

    Add API URL
    Added apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://ed9eb290.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
